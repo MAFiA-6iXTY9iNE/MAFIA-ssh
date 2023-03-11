1e1edb7bac99fae44009ae24b775f152df36e21e
Pasindu Tharusha <115170675+MAFiA-6iXTY9iNE@users.noreply.github.com>
2023-03-11 20:34:29 +0530

    Add files via upload

---
+++ etc/UserManager.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#font colors
+
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+ENDCOLOR="\e[0m"
+
+clear
+
+#add users
+
+echo -ne "${YELLOW}Enter the username : "; read username
+echo -ne "Enter password (please use a strong password) : "; read password
+echo -ne "Enter No. of Days till expiration : ";read nod
+exd=$(date +%F  -d "$nod days")
+useradd -e $exd -M -N -s /bin/false $username && echo "$username:$password" | chpasswd &&
+clear &&
+echo -e "${GREEN}User Detail" &&
+echo -e "${RED}-----------" &&
+echo -e "${GREEN}\nUsername :${YELLOW} $username" &&
+echo -e "${GREEN}\nPassword :${YELLOW} $password" &&
+echo -e "${GREEN}\nExpire Date :${YELLOW} $exd ${ENDCOLOR}" ||
+echo -e "${RED}\nFailed to add user $username please try again.${ENDCOLOR}"
+
+#return to panel
+
+echo -e "\nPress Enter key to return to main menu"; read
+menu
