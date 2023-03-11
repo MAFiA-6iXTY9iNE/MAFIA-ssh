2a9148ec1caf8e0bcf2f374cb9f01cd9d6981489
Pasindu Tharusha <115170675+MAFiA-6iXTY9iNE@users.noreply.github.com>
2023-03-11 20:10:19 +0530

    Update UserManager.sh

--- etc/UserManager.sh
+++ etc/UserManager.sh
@@ -12,7 +12,7 @@ clear
 #add users
 
 echo -ne "${YELLOW}Enter the username : "; read username
-echo -ne "Enter password (please use a strong password) : "; read password;
+echo -ne "Enter password (please use a strong password) : "; read password
 echo -ne "Enter No. of Days till expiration : ";read nod
 exd=$(date +%F  -d "$nod days")
 useradd -e $exd -M -N -s /bin/false $username && echo "$username:$password" | chpasswd &&
